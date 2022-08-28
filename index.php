7b9919a3226ce6c39c257bb750b737243f1301e9
Zzyto <87463770+Zzyto@users.noreply.github.com>
2022-08-28 22:28:27 +0700

    Add files via upload
    файлы для сайта

---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+  <?php include('menu.php');?>
+    <main class="main" style="margin: 0; height: 100vh; width: 100vw"></main>
+    <script src="main.js"></script>
+  </body>
+</html>
